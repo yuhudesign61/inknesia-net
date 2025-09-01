85ee273199dfe2d8174cc2fc8f18c815e68b9150
yuhudesign61 <yuhudesign61@gmail.com>
2025-09-01 21:36:17 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -4,7 +4,7 @@ import { connect } from "cloudflare:sockets";
 
 // Variables
 const rootDomain = "yuhudesign61.workers.dev"; // Ganti dengan domain utama kalian
-const serviceName = "inknesia-net"; // Ganti dengan nama workers kalian
+const serviceName = "inknesia-ia"; // Ganti dengan nama workers kalian
 const apiKey = ""; // Ganti dengan Global API key kalian (https://dash.cloudflare.com/profile/api-tokens)
 const apiEmail = ""; // Ganti dengan email yang kalian gunakan
 const accountID = ""; // Ganti dengan Account ID kalian (https://dash.cloudflare.com -> Klik domain yang kalian gunakan)
